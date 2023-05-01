06077c38a9f513cc7d7904fa131352172f6d5825
Yuya Murata <yu.murata6@gmail.com>
2023-05-01 12:06:29 +0900

    5/1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,11 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     before_action :configure_permitted_parameters, if: :devise_controller?
-    def hello
-      render html: "hello,Fly.io!"
-    end
-
     protected
   
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
       end
     # ==========ここまで追加する==========
   
